488fe2fc70b6d0675485b7b410fb0497168acb3b
romashorodok <108732029+romashorodok@users.noreply.github.com>
2024-02-06 20:58:31 +0200

    ref: separate ast from parsing logic

--- worker/main.go
+++ worker/main.go
@@ -34,24 +34,9 @@ func main() {
 	}
 	defer file.Close()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	result := make(chan *Node)
-
-	go func() {
-		defer wg.Done()
-
-		for r := range result {
-			traverse(r, 0)
-		}
-	}()
-
 	selectors := []string{"blog-item"}
-	Parse(file, selectors, result)
-	close(result)
-
-	wg.Wait()
+	// selectors := []string{"AllNewsItemService"}
+	Parse(file, selectors)
 }
 
 // token -> lexem
@@ -84,6 +69,124 @@ type Node struct {
 	next *Node
 }
 
+type AstGenerator struct {
+	tagNames        map[string]*atomic.Int32
+	rootNode        *Node
+	nodes           []*Node
+	nodesMu         sync.Mutex
+	autoClosingNode bool
+	onTreeComplete  func(*Node)
+}
+
+func (t *AstGenerator) AppendOpenTag(node *Node) {
+	t.nodesMu.Lock()
+	defer t.nodesMu.Unlock()
+
+	if t.nodes == nil {
+		t.nodes = make([]*Node, 0)
+	}
+
+	tagNameCounter, tagNameExists := t.tagNames[node.Name]
+	if !tagNameExists {
+		var counter atomic.Int32
+		counter.Add(1)
+		t.tagNames[node.Name] = &counter
+	} else {
+		tagNameCounter.Add(1)
+	}
+
+	t.nodes = append(t.nodes, node)
+
+	if t.rootNode == nil {
+		t.rootNode = node
+	}
+}
+
+func (t *AstGenerator) nextNode() *Node {
+	if len(t.nodes) > 0 {
+		node := t.nodes[len(t.nodes)-1]
+		t.nodes = t.nodes[:len(t.nodes)-1]
+		return node
+	}
+	return nil
+}
+
+func (t *AstGenerator) CloseTag(closingNode *Node) {
+	t.nodesMu.Lock()
+	defer t.nodesMu.Unlock()
+
+	tagNameCounter, tagNameExist := t.tagNames[closingNode.Name]
+	if !tagNameExist {
+		return
+	}
+
+	tagNameCounter.Add(-1)
+	if tagNameCounter.Load() == 0 {
+		delete(t.tagNames, closingNode.Name)
+	}
+
+	targetNode := t.nextNode()
+
+	// If target node is not correspond to the closing node.
+	// It may be by inconsistent stack or somewhere was lost closing tag.
+	// And need to complete open tag anyway. But what to do with lost open tag?
+	for len(t.nodes) > 0 && targetNode.Name != closingNode.Name {
+		nodeToSkip := targetNode
+
+		targetNode = t.nextNode()
+
+		// Link lost node as the next node of the tree stack
+		if t.autoClosingNode {
+			targetNodeLastChild := lastChildOfNode(targetNode)
+			targetNodeLastChild.next = nodeToSkip
+		}
+
+		if targetNode.Name == closingNode.Name {
+			break
+		}
+	}
+
+	if targetNode.Name == closingNode.Name {
+		if parentNode := t.PendingNode(); parentNode != nil {
+			parentNodeLastChild := lastChildOfNode(parentNode)
+			parentNodeLastChild.next = targetNode
+		}
+	}
+
+	if targetNode != nil && t.rootNode == targetNode {
+		t.onTreeComplete(t.rootNode)
+	}
+}
+
+func (t *AstGenerator) PendingNode() *Node {
+	if len(t.nodes) > 0 {
+		return t.nodes[len(t.nodes)-1]
+	}
+	return nil
+}
+
+func (t *AstGenerator) OnTreeComplete(fn func(*Node)) {
+	t.onTreeComplete = fn
+}
+
+func (t *AstGenerator) IsBuilding() bool {
+	return t.rootNode != nil
+}
+
+func (t *AstGenerator) Free() {
+	t.rootNode = nil
+	t.nodes = make([]*Node, 0)
+	t.tagNames = make(map[string]*atomic.Int32)
+}
+
+func NewAstGenerator() *AstGenerator {
+	return &AstGenerator{
+		tagNames:        make(map[string]*atomic.Int32),
+		nodes:           make([]*Node, 0),
+		autoClosingNode: true,
+	}
+}
+
 var OpenTagNames = make(map[string]*atomic.Int32)
 
 func getIndent(depth int) string {
@@ -108,11 +211,14 @@ func lastChildOfNode(node *Node) *Node {
 	return parentNode
 }
 
-func Parse(file io.Reader, classSelectors []string, result chan *Node) {
+func Parse(file io.Reader, classSelectors []string) {
 	tok := NewTokenizer(file)
 
-	var nodes []*Node
-	var rootNode *Node
+	ast := NewAstGenerator()
+	ast.OnTreeComplete(func(node *Node) {
+		ast.Free()
+		traverse(node, 0)
+	})
 
 	for {
 		token := tok.Next()
@@ -124,65 +230,28 @@ func Parse(file io.Reader, classSelectors []string, result chan *Node) {
 		switch t := token.(type) {
 		case *OpenTag:
 			node := &Node{Name: t.Name, Type: OPEN_NODE, Tag: t}
-			nodes = append(nodes, node)
 
-			tagNameCounter, tagNameExists := OpenTagNames[node.Name]
-			if !tagNameExists {
-				var counter atomic.Int32
-				counter.Add(1)
-				OpenTagNames[node.Name] = &counter
-			} else {
-				tagNameCounter.Add(1)
+			if ast.IsBuilding() {
+				ast.AppendOpenTag(node)
+				continue
 			}
 
-			if rootNode == nil && len(classSelectors) > 0 && containsClass(t.Attr["class"], classSelectors) {
-				rootNode = node
+			if len(classSelectors) > 0 && containsClass(t.Attr["class"], classSelectors) {
+				ast.AppendOpenTag(node)
 			}
 
 		case *CloseTag:
-			if len(nodes) <= 0 || rootNode == nil {
+			if ast.PendingNode() == nil {
 				continue
 			}
-
-			if _, tagNameExists := OpenTagNames[t.Name]; !tagNameExists {
-				continue
-			}
-
-			childNode := nodes[len(nodes)-1]
-			nodes = nodes[:len(nodes)-1]
-
-			for len(nodes) > 0 && childNode.Name != t.Name {
-				skippedNode := childNode
-
-				childNode = nodes[len(nodes)-1]
-				lastChild := lastChildOfNode(childNode)
-				lastChild.next = skippedNode
-
-				nodes = nodes[:len(nodes)-1]
-				if childNode.Name == t.Name {
-					break
-				}
-			}
-
-			if childNode.Name == t.Name {
-				if len(nodes) > 0 {
-					parentNode := nodes[len(nodes)-1]
-					lastChild := lastChildOfNode(parentNode)
-					lastChild.next = childNode
-				}
-			}
-
-			if childNode != nil && rootNode == childNode {
-				result <- rootNode
-				rootNode = nil
-				nodes = make([]*Node, 0)
-			}
+			ast.CloseTag(&Node{Name: t.Name, Type: CLOSE_NODE})
 
 		case *Text:
-			if len(nodes) > 0 {
-				childNode := nodes[len(nodes)-1]
-				childNode.Content = string(removeNewLine(t.Data))
+			pendingNode := ast.PendingNode()
+			if pendingNode == nil {
+				continue
 			}
+			pendingNode.Content = string(removeNewLine(t.Data))
 
 		case *Comment:
 		}
